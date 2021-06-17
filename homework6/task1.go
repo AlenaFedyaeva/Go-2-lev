87ce652a3de5120a9ccb7e3e1d215bb71c623a23
alenafedyaeva <291407@gmail.com>
2021-06-17 22:02:52 +0300

    hw 6

--- homework6/task1.go
+++ homework6/task1.go
@@ -2,42 +2,48 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"runtime/trace"
 	"sync"
 	"time"
 )
-  
-  const count = 1000
-  
-  func main() {
+
+// GOMAXPROCS=1 go run task1.go 2>traceTask1.out
+// go tool trace traceTask1.out
+const count = 1000
+
+func main() {
+	trace.Start(os.Stderr)
+	defer trace.Stop()
+	
 	var (
-	   counter int
-	   mutex sync.Mutex
-  
-	   // Вспомогательная часть нашего кода
-	   ch = make(chan struct{}, count)
+		counter int
+		mutex   sync.Mutex
+
+		// Вспомогательная часть нашего кода
+		ch = make(chan struct{}, count)
 	)
 	for i := 0; i < count; i += 1 {
-	   go func() {
-		  // Захват мьютекса
-		  mutex.Lock()
-		  counter += 1
-		  // Освобождение мьютекса
-		  mutex.Unlock()
-  
-		  // Фиксация факта запуска горутины в канале
-		  ch <- struct{}{}
-	   }()
+		go func() {
+			// Захват мьютекса
+			mutex.Lock()
+			counter += 1
+			// Освобождение мьютекса
+			mutex.Unlock()
+
+			// Фиксация факта запуска горутины в канале
+			ch <- struct{}{}
+		}()
 	}
-	time.Sleep(2*time.Second)
+	time.Sleep(2 * time.Second)
 	close(ch)
-  
+
 	i := 0
 	for range ch {
-	   i += 1
+		i += 1
 	}
 	// Выводим показание счетчика
 	fmt.Println(counter)
 	// Выводим показания канала
 	fmt.Println(i)
-  }
-  
+}
